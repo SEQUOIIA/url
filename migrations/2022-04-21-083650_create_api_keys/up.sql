7d9bcc6b57fa5d37d8629aa10c628fc06e72c8e1
Emil H. Clausen <sequoiia@hummel.yt>
2022-06-16 21:30:34 +0200

    Fixed invalid up.sql for api_keys table

--- migrations/2022-04-21-083650_create_api_keys/up.sql
+++ migrations/2022-04-21-083650_create_api_keys/up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS api_keys (
-                                    id BIGINT NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                    id BIGINT NOT NULL PRIMARY KEY,
                                     key TEXT NOT NULL UNIQUE,
                                     description TEXT
 );
